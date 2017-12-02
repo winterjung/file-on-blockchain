41ed52b1ccb6c028ee275cc535e66c03097715ae
jungwinter <wintermy201@gmail.com>
2017-12-02 23:07:36 +0900

    Change datedir path of geth script
    Signed-off-by: jungwinter <wintermy201@gmail.com>

--- blockchain/start.sh
+++ blockchain/start.sh
@@ -1,1 +1,1 @@
-geth --rpc --rpccorsdomain "*" --nodiscover --datadir "/Users/WinterJ/Github/pdf-on-blockchain/chain-data" --port "30303" --rpcapi "db,eth,net,web3,personal,admin,miner,debug,txpool" --networkid 1001012
+geth --rpc --rpccorsdomain "*" --nodiscover --datadir "chain-data" --port "30303" --rpcapi "db,eth,net,web3,personal,admin,miner,debug,txpool" --networkid 1001012
